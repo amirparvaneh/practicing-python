4296c23ec9b9a02239b1b652d3cb1dc4ec15860e
a.parvaneh <amirparvaneh138@gmail.com>
2025-03-25 00:00:44 +0330

    what's your name for hackerrank

---
+++ hackerrank/list_compression.py
@@ -0,0 +1,8 @@
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
+    
+    
+    
+    
---
+++ hackerrank/prin_name.py
@@ -0,0 +1,8 @@
+first_name = input()
+last_name = input()
+
+def print_full_name(first, last):
+    print("Hello " + first + " " + last + "! You just delved into python.")
+    
+print(print_full_name(first_name,last_name))
+    
