0ef132d4b5a1427ad6797e3b909c6a18fd392c34
a.parvaneh <amirparvaneh138@gmail.com>
2025-03-24 06:59:00 +0330

    optimization

--- hackerrank/power2.py
+++ hackerrank/power2.py
@@ -7,3 +7,14 @@ for i in range(n):
 
 for o in my_list:
     print(o)    
+    
+    
+# use deepseek to optimize the above code
+# n = int(input("Enter a number: "))
+
+# # Use list comprehension to create the list of squares
+# my_list = [i * i for i in range(n)]
+
+# # Print each item in the list
+# for o in my_list:
+#     print(o)    
